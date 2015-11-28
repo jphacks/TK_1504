5d15ae9b0b3578a62596b196c9a89ea29f6c5caa
UekusaTomoki <tmk-mac@tmk-mac.local>
2015-11-29 06:41:06 +0900

    Modified design and Import icons

--- src2/frontend/js/main.js
+++ src2/frontend/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
     console.log("initialized");
+
+    var vueMain = new Vue({
+        el:'.top-wrapper',
+        data:{
+            started:false
+        }
+    });
     $('.btn-start').click(function(){
-        console.log("click");
-    	$(".top-wrapper").toggleClass('anime-active');
+        if(!vueMain.started){
+            setTimeout(function(){
+        	    $(".top-wrapper").toggleClass('anime-active');
+            },2000);
+        }
+        vueMain.started = true;
     });
 });
